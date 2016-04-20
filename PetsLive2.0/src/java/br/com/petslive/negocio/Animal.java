31fb722f8f2881b2510b8f92cf90d15de95e50aa
Erick Leandro <Erick Leandro>
2016-04-20 14:13:23 -0300

    Terminando a classe animal

--- PetsLive2.0/src/java/br/com/petslive/negocio/Animal.java
+++ PetsLive2.0/src/java/br/com/petslive/negocio/Animal.java
@@ -31,4 +31,107 @@ public class Animal {
     @Column (name = "sexo")
     private char sexo;
     
+    public Animal(int id_animal, String nome, String especie, String raca, int idade, char sexo){
+        this.id_animal = id_animal;
+        this.nome_animal = nome;
+        this.especie = especie;
+        this.raca = raca;
+        this.idade = idade;
+        this.sexo = sexo;
+        
+    }
+    
+    public Animal(){
+    }
+    
+    
+    
+    
+    
+    /**
+     * @return the id_animal
+     */
+    public int getId_animal() {
+        return id_animal;
+    }
+
+    /**
+     * @param id_animal the id_animal to set
+     */
+    public void setId_animal(int id_animal) {
+        this.id_animal = id_animal;
+    }
+
+    /**
+     * @return the nome_animal
+     */
+    public String getNome_animal() {
+        return nome_animal;
+    }
+
+    /**
+     * @param nome_animal the nome_animal to set
+     */
+    public void setNome_animal(String nome_animal) {
+        this.nome_animal = nome_animal;
+    }
+
+    /**
+     * @return the especie
+     */
+    public String getEspecie() {
+        return especie;
+    }
+
+    /**
+     * @param especie the especie to set
+     */
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    /**
+     * @return the raca
+     */
+    public String getRaca() {
+        return raca;
+    }
+
+    /**
+     * @param raca the raca to set
+     */
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    /**
+     * @return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public char getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+    
+    
+    
 }
