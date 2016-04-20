ed109175cf99e44979c96ce4e4fdccda67c4e50c
Erick Leandro <Erick Leandro>
2016-04-20 14:03:22 -0300

    construção de animal

--- PetsLive2.0/src/java/br/com/petslive/negocio/Animal.java
+++ PetsLive2.0/src/java/br/com/petslive/negocio/Animal.java
@@ -5,10 +5,30 @@
  */
 package br.com.petslive.negocio;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author Erick
  */
+@Entity
+@Table
 public class Animal {
+    @Id
+    @Column (name = "id_animal",nullable = false)
+    private int id_animal;
+    @Column (name = "nome_animal", nullable = false, length = 100)
+    private String nome_animal;
+    @Column (name = "especie", length = 50)
+    private String especie;
+    @Column (name = "raca", length = 50)
+    private String raca;
+    @Column (name = "idade") 
+    private int idade;
+    @Column (name = "sexo")
+    private char sexo;
     
 }
